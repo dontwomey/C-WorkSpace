358979f7db4c3e595d3d30d61353fd9a9ee9ac04
Donagh Twomey <donagh.twomey@gmail.com>
2019-11-02 18:59:26 +0000

    more code

---
+++ XMLReader/XMLReader/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml;
+using System.Linq;
+using System.Xml.Linq;
+using System.IO;
+using System.Text;
+using System.Data;
+
+namespace XMLReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load("breakfast_menu.xml");
+
+            XmlNodeList elemlist = doc.GetElementsByTagName("name");
+         
+            foreach (XmlNode node in elemlist)
+            {
+                if (node.InnerXml.ToLower().StartsWith('b'))
+                {
+                    Console.WriteLine(node.InnerXml);
+                }
+               
+            }
+        }
+    }
+}
