ce0a94e5aa8042da257c33fb8e1165c0734ce360
Donagh Twomey <donagh.twomey@gmail.com>
2019-11-02 12:44:18 +0000

    Testing code commit

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "TEstSEt";
+
+            Console.WriteLine(isPallindrome1(word));
+            Console.WriteLine(isPallindrome2(word));
+
+        }
+
+        private static bool isPallindrome2(string word)
+        {
+            word = word.ToLower();
+            bool flag = true;
+            
+            for (int i = 0; i < word.Length / 2 ; i++){
+                if (word[i] != word[word.Length - i - 1])
+                {
+                    flag = false;
+                }
+            }
+
+            return flag;
+        }
+
+        private static bool isPallindrome1(string word)
+        {
+            char[] reversearr = word.ToCharArray();
+            Array.Reverse(reversearr);
+            string reverseword = new string(reversearr);
+
+            if (word.ToLower() == reverseword.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
